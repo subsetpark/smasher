14846319a1112f38d32ae5915202935b167716a6
Z. D. Smith <cruxdestruct@gmail.com>
2014-11-20 16:55:41 -0500

    Initial commit

---
+++ atoms.py
@@ -0,0 +1,49 @@
+class Atom(Exception):
+    pass
+
+def new_atom(atom):
+    return type(atom, (Atom,), {})
+
+def atom_try(atom_map):
+    def inner_wrapper(func):
+        def wrapped(self, *args, **kwargs):
+            try:
+                func(self, *args, **kwargs)
+            except Exception as exc:
+                exc_name = exc.__class__.__name__
+                if exc_name in atom_map:
+                    route = atom_map[exc_name]
+                    if route in self.atoms:
+                        self.pass_atom(atom_map[exc_name])
+                    elif isinstance(route, str):
+                        getattr(self, route)()
+                    else:
+                        route()
+                else:
+                    raise exc
+        return wrapped
+    return inner_wrapper
+
+def atom_raise(atom):
+    raise atom()
+
+class AtomPasser:
+    def __init__(self):
+        for atom in self.atoms:
+            new_obj = new_atom(atom)
+            setattr(self, atom, new_obj)
+
+    def pass_atom(self, atom):
+        raise getattr(self, atom)()
+
+    def check(self, cond, false_atom, true_atom=None):
+        if not cond:
+            self.pass_atom(false_atom)
+        if true_atom:
+            self.pass_atom(true_atom)
+
+class Atoms:
+    def __init__(self, *atoms):
+        for atom in atoms:
+            new_obj = new_atom(atom)
+            setattr(self, atom, new_obj)
---
+++ game.py
@@ -0,0 +1,24 @@
+from atoms import AtomPasser, atom_try, atom_raise
+import random
+
+class SimpleGame(AtomPasser):
+    atoms = ['Correct', 'Wrong', 'Win']
+
+    def __init__(self):
+        super().__init__()
+        self.points = 0
+        self.number = random.choice(range(10))
+        print(self.number)
+    
+    @atom_try({'Correct': lambda: print('Good job!')})
+    def play_a_game(self):
+        self.take_a_guess()
+
+    @atom_try({'Wrong': 'take_a_guess', 'ValueError': 'take_a_guess', 'Again': 'take_a_guess'})
+    def take_a_guess(self):
+        guess = input('Take a guess: ')
+        self.check(int(guess) == self.number, 'Wrong', 'Correct')
+
+if __name__ == '__main__':
+    g = SimpleGame()
+    g.play_a_game()
