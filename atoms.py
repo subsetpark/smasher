9bffd8257750f98356ba3b3392b345b2d921d4d5
Z. D. Smith <cruxdestruct@gmail.com>
2014-11-21 07:31:53 -0800

    working recursive decorator

--- atoms.py
+++ atoms.py
@@ -1,8 +1,11 @@
 class Atom(Exception):
-    pass
+    def __str__(self):
+        return 'Atom: {}'.format(self.__class__.__name__)
+
 
 def new_atom(atom):
     return type(atom, (Atom,), {})
+
 
 def atom_try(entity_map):
     atom_map = {}
@@ -12,6 +15,7 @@ def atom_try(entity_map):
         else:
             for atom in entity:
                 atom_map[atom] = entity_map[entity]
+
     def inner_wrapper(func):
         def wrapped(self, *args, **kwargs):
             try:
@@ -20,22 +24,22 @@ def atom_try(entity_map):
                 exc_name = exc.__class__.__name__
                 if exc_name in atom_map:
                     route = atom_map[exc_name]
-                    self.evaluate_msg(route)
+                    atom_try(entity_map)(self.__class__.evaluate_msg)(self, route)
                 else:
                     raise exc
         return wrapped
     return inner_wrapper
 
+
 class AtomPasser:
+
     def __init__(self):
-        self.current_atom = None
         for atom in self.atoms:
             new_obj = new_atom(atom)
             setattr(self, atom, new_obj)
 
     def pass_atom(self, atom):
-        self.current_atom = getattr(self, atom)
-        raise self.current_atom()
+        raise getattr(self, atom)()
 
     def evaluate_msg(self, msg):
         if msg in self.atoms:
@@ -45,16 +49,15 @@ class AtomPasser:
         else:
             msg()
 
-    def reraise(self):
-        raise self.current_atom()
-
     def check(self, cond, false_atom, true_atom=None):
         if not cond:
             self.evaluate_msg(false_atom)
         if true_atom:
             self.evaluate_msg(true_atom)
 
+
 class Atoms:
+
     def __init__(self, *atoms):
         for atom in atoms:
             new_obj = new_atom(atom)
--- game.py
+++ game.py
@@ -1,28 +1,30 @@
 from atoms import AtomPasser, atom_try
 import random
 
+
 class SimpleGame(AtomPasser):
-    atoms = ['Correct', 'Wrong', 'Win']
+    atoms = ['Correct', 'Wrong', 'Win', 'KeepGoing', 'Done']
 
     def __init__(self):
         super().__init__()
         self.points = 0
+
+    def play_a_game(self):
         self.number = random.choice(range(10))
         print(self.number)
-    
-    @atom_try({'Correct': lambda: print('Good job!')})
-    def play_a_game(self):
         self.take_a_guess()
 
-    @atom_try({('Wrong', 'ValueError', 'Again'): 'take_a_guess', 
-                'Correct': 'correct'})
+    @atom_try({('Wrong', 'ValueError', 'Again'): 'take_a_guess',
+               'Correct': 'correct',
+               'KeepGoing': 'play_a_game',
+               'Done': lambda: print('Good job!')})
     def take_a_guess(self):
         guess = input('Take a guess: ')
         self.check(int(guess) == self.number, 'Wrong', 'Correct')
 
     def correct(self):
         self.points += 1
-        self.check(self.points >= 5, 'take_a_guess', 'Correct')
+        self.check(self.points >= 5, 'KeepGoing', 'Done')
 
 if __name__ == '__main__':
     g = SimpleGame()
